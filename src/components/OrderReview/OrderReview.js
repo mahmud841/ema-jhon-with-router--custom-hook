ecff22be942109ba01c67768774339b31040cd4b
AL MAHMUD SURUJ <mahmudsuruj14@gmail.com>
2021-10-02 03:40:16 +0800

    dynamic content

--- src/components/OrderReview/OrderReview.js
+++ src/components/OrderReview/OrderReview.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import useCart from '../../hooks/useCart/useCart';
 import useProducts from '../../hooks/usePd';
 import { removeFromDb } from '../../utilities/fakedb';
@@ -6,33 +7,39 @@ import Cart from '../Cart/Cart';
 import ReviewItem from '../RiviewItem/ReviewItem';
 
 const OrderReview = () => {
-    const [products,setProducts ] = useProducts();
+    const [products] = useProducts();
     const [cart, setCart] = useCart(products);
     const handleRemove = key => {
         // console.log(key);
         const newCart = cart.filter(product => product.key !== key);
-        setCart (newCart);
+        setCart(newCart);
         removeFromDb(key);
 
-        
+
     }
 
     return (
         <div className="shop-container">
             <div className="product-container">
                 {
-                    cart.map(product =><ReviewItem 
+                    cart.map(product => <ReviewItem
                         key={product.key}
                         product={product}
-                        handleRemove = {handleRemove}
-                        ></ReviewItem> )
-                } 
+                        handleRemove={handleRemove}
+                    ></ReviewItem>)
+                }
 
             </div>
             <div className="cart-container">
-                <Cart cart={cart}></Cart>
+                <Cart cart={cart}>
+                    {/* <h1>Hello OrderReview </h1> */}
+                    <Link to ="/review">
+                        <button className="btn-regular">Place Order</button>
+                    </Link>
+                </Cart>
+
             </div>
-            
+
         </div>
     );
 };
